e53e84faebf80402872411f6fc8663d778fb8d45
Sharad Baidya <baidyasharad@gmail.com>
2016-02-14 12:39:33 +0545

    changes in file

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var app = angular.module('app',['ngRoute', 'ngCookies']);
+var app = angular.module('app',[
+                                 'ngRoute',
+                                 'ngCookies'
+                                ]
+                        );
+
 app.config(function configure($routeProvider) {
 
 	$routeProvider
 		         .when('/', 
 		         	      { 
 		         	      	controller: 'UsersController', 
-		         	      	templateUrl: './templates/customers.html' 
+		         	      	templateUrl: 'home/home.html' 
 		         	      }
 		         	  )
-             .when('/user/:user_id',
-                         {
-                           
-                            controller: 'UserController', 
-                            templateUrl: './templates/customer.html' 
-
-                         }
-                      )
+           
              .when('/login',
                          {
                            
                             controller: 'LoginController', 
-                            templateUrl: './templates/login.html' 
+                            templateUrl: 'login/login.html' 
 
                          }
                       )
@@ -34,15 +32,49 @@ app.config(function configure($routeProvider) {
               .otherwise({ redirect: '/' });
 });
 
+app.run(function() {
+  
+    
+     
+
+});
 
 app.factory('Data', function Data($http) {
 
 	return {
 
-		  
+		   getUsers: function getUsers() { 
+
+		   	                 return $http.get('/angular/public/users'); 
+		   	             },
+
+		   getUser: function getUser(id) { 
+
+		   	                 return $http.get('/angular/public/users/'+ id); 
+		   	             },
+		   addUser: function addUser(data) { 
+
+		   	                 return $http.post('/angular/public/users', data); 
+		   	             },
+		   removeUser: function removeUser(id) { 
+
+		   	                 return $http.delete('/angular/public/users/'+ id); 
+		   	             },
+           
+       addTrans : function(data){
+
+                         return $http.post('/angular/public/transactions',data);
+                        
+                     },
+
+       removeTrans: function(id) { 
+
+                         return $http.delete('/angular/public/transactions/'+ id); 
+                     },
+
        validateUser: function(data) { 
 
-                         return $http.post('/public/validate-user',data); 
+                         return $http.post('/angular/public/validate-user',data); 
                      },
 
        }
@@ -51,44 +83,169 @@ app.factory('Data', function Data($http) {
 });
 
 
+app.controller('UsersController', function UsersController($scope,$cookieStore,$location,Data) {
+    
+    (function(){
+             
 
-app.controller('LogoutController',function LogoutController($scope,$cookieStore,$location){
+             if(typeof $cookieStore.get('user_id') === 'undefined'){
+ 
+                    $location.path('/login');
+              }  
+
+    })()
+
+  
+    Data.getUsers().success(function(data){
+
+    	  //console.log(data);
+    	  $scope.users = data;
+    });
+     
+   
+     
+    $scope.newUser = { username: ''};
+    $scope.addUser = function() {
        
-       $cookieStore.remove("user_id");
-       $location.path('/');
+        var username = $scope.newUser.username;
+        Data.addUser({
+            
+            username: username,
+            
+        })
+        .success(UserAddSuccess).error(UserAddError);
+        
+        
+    }
+     
+    function UserAddSuccess(data) {
+        $scope.error = null;
+        $scope.users.push(data);
+        $scope.newUser = { username: '' };
 
+    }
+ 
+    function UserAddError(data) {
+        $scope.error = "Oops Something went wrong";
+    }
+     
+    $scope.removeUser = function(id) {
+   
+        if (confirm('Do you really want to remove this customer?')) {
+                
+                Data.removeUser(id).success(userRemoveSuccess);
+        
+        }
+    }
+     
+    function userRemoveSuccess(data) {
+        
+        var i = $scope.users.length;
+        while (i--) {
+            
+            if ($scope.users[i].id == data) {
+                
+                  $scope.users.splice(i, 1);
+            
+            }
+        }
+    
+    }
+
+
+   
 });
 
 
-app.controller('LoginController', function UsersController($scope,$cookieStore,$location,Data) {
+app.controller('UserController', function UsersController($scope,$cookieStore,$routeParams,$location,Data) {
     
-         $scope.validateData = { 'username' : '','password':'' };
-         $scope.validate = function(){
-              
-             
-              Data.validateUser($scope.validateData)
-                     .success(function(data){
+         (function(){
+            
+              if(typeof $cookieStore.get('user_id') === 'undefined'){
+ 
+                    $location.path('/login');
+              }
 
-                             if(typeof data.id === 'undefined'){
-                                   
-                                 $scope.error = "Invalid Username or password";
-                                 $scope.validateData.password = "";
-                                 $location.path('/login');
-                             
-                             }
-                             else{
+         })()
+         
+         $scope.counter = 0;
+         $scope.newTransaction = {
+                                 'amount' : '',
+                                 'description' : '',
+                                 'brokers':[
 
-                                $cookieStore.put('user_id',data.id);
-                                $location.path('/');
-                             }
-                     })
-                     .error(function(data){
-                         
-                          $scope.error = "Oops something went wrong";
+                                           ],
+                               
 
-                     })
-              
+                              }
+         var getTrans = function(){                     
+                 
+                 Data.getUser($routeParams.user_id)
+                            .success(function(data){
+                                  
+                                  //alert(JSON.stringify(data));
+                                  $scope.user = data;
+                                  $scope.transactions = data.transactions;
+                                  
+                                 
+                            })
+                            .error(function(data){
+                                
+                                  
+                                  $scope.error = "Oops Something went wrong";
+                                 
+                            });
+        
+        
+         }
+
+         getTrans();
+         
+         $scope.addTrans =  function(){
+                 
+                 $scope.newTransaction.user_id = $routeParams.user_id;
+                 
+                 Data.addTrans($scope.newTransaction)
+                               .success(function(data){
+                                     
+                                     alert(JSON.stringify($scope.newTransaction));
+                                     $scope.transactions.push(data);
+                                     $scope.datas.push(data);
+                                     $scope.newTransaction = {
+                                         'amount' : '',
+                                         'description' : '',
+                                          'brokers':[
+
+                                                    ],
+                               
+                                     }
+                               })
+                               .error(function(data){
+
+                                    $scope.error = "Opps! Somethig went wrong";
+                               })
+                  
+                 
+         }
+
+         $scope.removeTrans =  function(id){
+                
+                
+                Data.removeTrans(id)
+                               .success(function(data){
+
+                                     getTrans();
+
+                                     
+                               })
+                               .error(function(data){
+
+                                    $scope.error = "Opps! Somethig went wrong";
+                               })
+                 
+
 
          }
-   
+         
+
 });
