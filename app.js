6c1cebe5d01aac6b42dfad3879be109148effd57
Sharad Baidya <baidyasharad@gmail.com>
2016-02-14 12:01:22 +0545

    Login with angularjs

---
+++ app.js
@@ -0,0 +1,95 @@
+var app = angular.module('app',['ngRoute', 'ngCookies']);
+
+app.config(function configure($routeProvider) {
+
+	$routeProvider
+		         .when('/', 
+		         	      { 
+		         	      	controller: 'UsersController', 
+		         	      	templateUrl: './templates/customers.html' 
+		         	      }
+		         	  )
+             .when('/user/:user_id',
+                         {
+                           
+                            controller: 'UserController', 
+                            templateUrl: './templates/customer.html' 
+
+                         }
+                      )
+             .when('/login',
+                         {
+                           
+                            controller: 'LoginController', 
+                            templateUrl: './templates/login.html' 
+
+                         }
+                      )
+             .when('/logout',
+                          {
+                               
+                             controller: 'LogoutController',
+                             template: " ",  
+                          }
+                  )
+              .otherwise({ redirect: '/' });
+});
+
+
+app.factory('Data', function Data($http) {
+
+	return {
+
+		  
+       validateUser: function(data) { 
+
+                         return $http.post('/public/validate-user',data); 
+                     },
+
+       }
+
+
+});
+
+
+
+app.controller('LogoutController',function LogoutController($scope,$cookieStore,$location){
+       
+       $cookieStore.remove("user_id");
+       $location.path('/');
+
+});
+
+
+app.controller('LoginController', function UsersController($scope,$cookieStore,$location,Data) {
+    
+         $scope.validateData = { 'username' : '','password':'' };
+         $scope.validate = function(){
+              
+             
+              Data.validateUser($scope.validateData)
+                     .success(function(data){
+
+                             if(typeof data.id === 'undefined'){
+                                   
+                                 $scope.error = "Invalid Username or password";
+                                 $scope.validateData.password = "";
+                                 $location.path('/login');
+                             
+                             }
+                             else{
+
+                                $cookieStore.put('user_id',data.id);
+                                $location.path('/');
+                             }
+                     })
+                     .error(function(data){
+                         
+                          $scope.error = "Oops something went wrong";
+
+                     })
+              
+
+         }
+   
+});
